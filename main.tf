ff57eec410fedde41a1ea85fca3c99f540aed93f
Jeremy Collins <jeremycollins@utah.gov>
2020-12-02 15:23:24 -0700

    added resource with example data using aws instance t2.micro server(free one for testing)

--- main.tf
+++ main.tf
@@ -9,8 +9,22 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  //set region depending on region in AWS
   region = "us-east-1"
   access_key = "${myAccessKey}"
   secret_key = "${mySecretKey}"
 }
+
+resource "aws_instance" "web" {
+  ami = "${amiTag}"
+  instance_type = "t2.micro"
+  
+}
+
+# resource "<provider>_<resource_type>" "name" {
+#   config options....
+#   key = "value"
+#   key2 = "another value"
+# }
+
 
